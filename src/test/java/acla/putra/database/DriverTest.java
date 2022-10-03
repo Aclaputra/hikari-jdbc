71b55f92e17c6257f51897322a57f0c71889ad66
aclaputra <muhamadacla22@gmail.com>
2022-10-04 04:55:50 +0700

    fix: fixing some typo on test

--- src/test/java/acla/putra/database/DriverTest.java
+++ src/test/java/acla/putra/database/DriverTest.java
@@ -1,4 +1,11 @@
-pakcage acla.putra.database;
+package acla.putra.database;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DriverTest
 {
@@ -8,11 +15,13 @@ public class DriverTest
   {
     try
     {
-      Driver mysqlDriver = new com.mysql.jc.jdbc.Driver();
+      Driver mysqlDriver = new com.mysql.cj.jdbc.Driver();
       DriverManager.registerDriver(mysqlDriver); 
     } 
     catch (SQLException exception)
     {
       Assertions.fail(exception);
     }
+
+  }
 }
