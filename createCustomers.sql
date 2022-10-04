5a7e11df2bbc72179e5104dce908d3b8b6d1f92e
aclaputra <muhamadacla22@gmail.com>
2022-10-05 04:09:05 +0700

    feat: added createCustomers.sql

---
+++ createCustomers.sql
@@ -0,0 +1,7 @@
+CREATE TABLE customers (
+  id VARCHAR(100) NOT NULL,
+  name VARCHAR(100) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  CONSTRAINT email_unique UNIQUE (email),
+  PRIMARY KEY (id)
+) ENGINE = InnoDB;
