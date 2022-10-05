daa6879b006cdbb3f53e94136af17e5ba99cf9e7
aclaputra <muhamadacla22@gmail.com>
2022-10-05 17:58:12 +0700

    feat: modified admin sql

--- admin.sql
+++ admin.sql
@@ -1,3 +1,9 @@
 CREATE TABLE admin (
+  username VARCHAR(100) NOT NULL,
+  password VARCHAR(100) NOT NULL,
+  PRIMARY KEY (username)
+) ENGINE = InnoDB;
 
-);
+INSERT INTO admin(username, password) VALUES ('admin', 'admin');
+
+SELECT * FROM admin;
