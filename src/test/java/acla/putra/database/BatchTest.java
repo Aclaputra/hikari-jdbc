6bb1615df1017d1e3f591a4dbe6884bfa3a9d7bb
aclaputra <muhamadacla22@gmail.com>
2022-10-08 05:46:37 +0700

    feat: added BatchTest

--- src/test/java/acla/putra/database/BatchTest.java
+++ src/test/java/acla/putra/database/BatchTest.java
@@ -3,12 +3,45 @@ package acla.putra.database;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
 
 public class BatchTest
 {
   @Test
-  void testStatement()
-  {
+  void testStatement() throws SQLException {
     Connection connection = ConnectionUtil.getDataSource().getConnection();
+    Statement statement = connection.createStatement();
+
+    String sql = "INSERT INTO comments(email, comment) VALUES ('acla@test.com', 'hi')";
+
+    for (int i = 0; i < 1000; i++) {
+      statement.addBatch(sql);
+    }
+
+    statement.executeBatch();
+
+    statement.close();
+    connection.close();
+  }
+
+  @Test
+  void testPreparedStatement() throws SQLException {
+    Connection connection = ConnectionUtil.getDataSource().getConnection();
+    String sql = "INSERT INTO comments(email, comment) VALUES (?, ?)";
+    PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+    for (int i = 0; i < 1000; i++) {
+      preparedStatement.clearParameters();
+      preparedStatement.setString(1, "acla@test.com");
+      preparedStatement.setString(2, "hi");
+      preparedStatement.addBatch();
+    }
+
+    preparedStatement.executeBatch();
+
+    preparedStatement.close();
+    connection.close();
   }
 }
